fa3da271916464d297477bb700c70b2408e23710
Judy <143587407+Judy770@users.noreply.github.com>
2024-01-26 14:22:10 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,17 @@
+function generateRandomData() {
 
-        for (int i = 0; i < 10; i++) {
-}
-public class RandomDataGenerator {
 
-        }
-            System.out.println("Random Number: " + item);
-    public static void main(String[] args) {
+function main() {
+    const data = generateRandomData();
+    return data;
+    data.forEach(item => {
+main();
     }
-        int[] data = new int[10];
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
+}
+
+    const data = [];
+        console.log("Random Number: " + item);
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+    for (let i = 0; i < 10; i++) {
